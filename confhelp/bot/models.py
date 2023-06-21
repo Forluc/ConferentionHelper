18ec830e43a160a1509c260ed1604813151f353a
Sammels <Sasagete@gmail.com>
2023-06-22 01:16:35 +0300

    Внес дополнения для модели

--- confhelp/bot/models.py
+++ confhelp/bot/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+import datetime
 
-# Create your models here.
+
+class EventCalendar(models.Model):
+    event_date_start = models.DateTimeField()
+    event_date_end = models.DateTimeField()
+    reporter = models.IntegerField()
+    speech_topic = models.TextField()
+
+
+class EventDate(models.Model):
+    event_time_start = models.ForeignKey(EventCalendar, on_delete=models.CASCADE)
+    event_time_stop = models.ForeignKey(EventCalendar, on_delete=models.CASCADE)
+    
+
+
+
+
--- confhelp/confhelp/settings.py
+++ confhelp/confhelp/settings.py
@@ -1,3 +1,5 @@
+import os
+
 from pathlib import Path
 from environs import Env
 
@@ -69,7 +71,7 @@ WSGI_APPLICATION = 'confhelp.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
     }
 }
 
