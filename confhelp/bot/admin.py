c7dda3c1417faaffcfe3e56924bb1538f07ba5cd
Vladimir <vladimir.papin@gmail.com>
2023-06-24 11:55:51 +0300

    create models

--- confhelp/bot/admin.py
+++ confhelp/bot/admin.py
@@ -1,3 +1,94 @@
 from django.contrib import admin
+from .models import MeetUpUser, Speach, Qustion
+from django.utils.html import format_html
+from django.db.models import Count
 
-# Register your models here.
+@admin.register(MeetUpUser)
+class MeetUpUserAdmin(admin.ModelAdmin):
+    list_display = [
+        'get_meet_up_user_full_name',
+        'get_meet_up_user_email',
+        'phone',
+        'is_speaker',
+        ]
+    list_filter = ['is_speaker',]
+    search_fields = ('user__email',
+                     'user__first_name',
+                     'user__last_name',
+                     'phone')
+
+
+    def get_meet_up_user_full_name(self, obj):
+        return f'{obj.user.first_name} {obj.user.last_name}'
+
+    get_meet_up_user_full_name.short_description = 'Пользователь'
+
+
+    def get_meet_up_user_email(self, obj):
+        return obj.user.email
+
+    get_meet_up_user_email.short_description = 'Почта'
+
+
+@admin.register(Speach)
+class SpeachAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'get_speaker',
+        'start_at',
+        'end_at',
+        ]
+
+    def get_speaker(self, obj):
+        return f'{obj.speaker.user.first_name} {obj.speaker.user.last_name}'
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "speaker":
+            kwargs["queryset"] = MeetUpUser.objects.filter(is_speaker=True)
+        return super().formfield_for_foreignkey(db_field, request, **kwargs)
+
+
+@admin.register(Qustion)
+class QustionAdmin(admin.ModelAdmin):
+    list_display = [
+        'get_title',
+        'get_speach',
+        'get_user_count',]
+
+    raw_id_fields = ['speach', 'user',]
+
+
+    def get_title(self, obj):
+        question = obj.title
+        if len(question) > 50:
+            return f'{question[:50]}...'
+
+        return question
+
+    get_title.short_description = 'Вопрос'
+
+
+    def get_queryset(self, request):
+        queryset = super().get_queryset(request)
+        queryset = queryset.annotate(
+            _user_count=Count("user", distinct=True),
+        )
+        return queryset
+
+
+    def get_speach(self, obj):
+        speach = obj.speach.title
+        if len(speach) > 50:
+            return f'{speach[:50]}...'
+
+        return speach
+
+    get_speach.short_description = 'К докладу'
+
+
+    def get_user_count(self, obj):
+        return format_html("<b><i>{}</i></b>", obj._user_count)
+
+    get_user_count.short_description = 'Кол-во вопросов'
+    get_user_count.admin_order_field = '_user_count'
+    get_title.admin_order_field = 'title'
--- confhelp/bot/models.py
+++ confhelp/bot/models.py
@@ -1,19 +1,74 @@
 from django.db import models
+from django.contrib.auth.models import User
+from phonenumber_field.modelfields import PhoneNumberField
 import datetime
 
 
-class EventCalendar(models.Model):
-    event_date_start = models.DateTimeField()
-    event_date_end = models.DateTimeField()
-    reporter = models.IntegerField()
-    speech_topic = models.TextField()
+class MeetUpUser(models.Model):
+    user = models.OneToOneField(
+        User,
+        on_delete=models.CASCADE,
+        verbose_name='Пользователь')
+
+    telegram_id = models.CharField(
+        'Telegram id',
+        max_length=50,
+        db_index=True)
+
+    is_speaker = models.BooleanField(
+        default=False,
+        verbose_name='Докладчик')
+
+    phone = PhoneNumberField(
+        'Номер телефона',
+        null=True,
+        blank=True,
+        db_index=True)
 
 
-class EventDate(models.Model):
-    event_time_start = models.ForeignKey(EventCalendar, on_delete=models.CASCADE)
-    event_time_stop = models.ForeignKey(EventCalendar, on_delete=models.CASCADE)
-    
+    def __str__(self):
+        return f'{self.user.first_name} {self.user.last_name}'
 
 
+class Speach(models.Model):
+    title = models.CharField(
+        'Наименование',
+        max_length=200,)
+
+    description = models.TextField(
+        'Описание', blank=True)
+
+    speaker = models.ForeignKey(
+        MeetUpUser,
+        on_delete=models.CASCADE,
+        related_name='speachs',
+        verbose_name='Спикер')
+
+    start_at = models.DateTimeField(
+        verbose_name='Начало')
+
+    end_at = models.DateTimeField(
+        verbose_name='Окончание')
 
 
+    def __str__(self):
+        return self.title
+
+
+class Qustion(models.Model):
+    title = models.TextField(
+        'Вопрос',)
+
+    speach = models.ForeignKey(
+        Speach,
+        on_delete=models.CASCADE,
+        related_name='qustions',
+        verbose_name='К докладу',)
+
+    user = models.ManyToManyField(
+        User,
+        related_name='qustions',
+        verbose_name='Кто задал вопрос')
+
+    def __str__(self):
+        return self.title
